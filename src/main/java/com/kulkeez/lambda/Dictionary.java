fe3cf245c28ca2af10f5134a3e1fd310e6595c1c
Vikram Kulkarni <kulkeez@yahoo.com>
2022-12-30 18:22:25 +0530

    removed redundant import

--- src/main/java/com/kulkeez/lambda/Dictionary.java
+++ src/main/java/com/kulkeez/lambda/Dictionary.java
@@ -3,10 +3,9 @@ package com.kulkeez.lambda;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import com.kulkeez.HelloController;
 
 import lombok.extern.slf4j.Slf4j;
 
