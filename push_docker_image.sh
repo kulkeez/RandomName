f9e162ce23f787302cc5a6dbe0560a6d0f7f47bc
Vikram Kulkarni <kulkeez@yahoo.com>
2024-05-31 11:56:04 +0000

    fixed incorrect packaging

--- push_docker_image.sh
+++ push_docker_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker tag simulap-api:0.1 hub.docker.hpecorp.net/simulap_devel/simulap-api:0.1
+#docker tag random-name:0.1 kulkeez/random-name:0.1
 echo "Publishing the Docker image to the public Docker Hub Registry ..."
 docker push kulkeez/random-name:0.1
 echo "Published the Docker image within the Public Docker Hub Registry!"
