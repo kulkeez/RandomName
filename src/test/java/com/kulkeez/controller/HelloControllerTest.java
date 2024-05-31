757860829e0d973f1b5333b8c3a87d1ed8679be3
Vikram Kulkarni <kulkeez@yahoo.com>
2024-05-31 12:55:15 +0000

    removed unused import

--- src/test/java/com/kulkeez/controller/HelloControllerTest.java
+++ src/test/java/com/kulkeez/controller/HelloControllerTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
