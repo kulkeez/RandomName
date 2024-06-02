20d4b4e2e4575113133d1b792233efdb76c7c73d
Vikram Kulkarni <vikram.kulkarni@hpe.com>
2024-06-02 19:52:57 +0530

    removed unused import

--- src/main/java/com/kulkeez/controller/StatusController.java
+++ src/main/java/com/kulkeez/controller/StatusController.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
